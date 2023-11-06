3e62716cf7cd9968a9cf7c348359099b9dfbf872
camille <camille.cliche1@gmail.com>
2023-11-06 08:58:35 +0100

    e

--- SQL/exemple-1.sql
+++ SQL/exemple-1.sql
@@ -36,8 +36,62 @@ SELECT `nom`, `titre` , `nodep` , `salaire` FROM employe WHERE `titre` = "repré
 SELECT `nom`, `titre` , `nodep` , `salaire` FROM employe WHERE `nodep` = 34 AND `titre` = "représentant" OR "secrétaire"
 
 12 - Afficher le nom, le titre, le numéro de département, le salaire des employés dont le titre est Représentant, ou dont le titre est Secrétaire dans le département numéro 34.
-.
+SELECT `nom`, `titre`, `nodep` FROM employe WHERE `nodep` = 34 AND `salaire`= `titre` = "représentant" OR `titre` = "secrétaire"
 
 13 - Afficher le nom, et le salaire des employés dont le salaire est compris entre 20000 et 30000
+SELECT `nom`, `salaire` FROM employe WHERE `salaire` BETWEEN 20000 AND 30000
+
+15 - Afficher le nom des employés commençant par la lettre « H »
 .
 
+16 - Afficher le nom des employés se terminant par la lettre « n ».
+.
+
+17 - Afficher le nom des employés contenant la lettre « u » en 3ème position.
+.
+
+18 - Afficher le salaire et le nom des employés du service 41 classés par salaire croissant.
+.
+
+19 - Afficher le salaire et le nom des employés du service 41 classés par salaire décroissant.
+.
+
+20 - Afficher le titre, le salaire et le nom des employés classés par titre croissant et par salaire décroissant.
+.
+
+21 - Afficher le taux de commission, le salaire et le nom des employés classés par taux de commission croissante.
+.
+
+22 - Afficher le nom, le salaire, le taux de commission et le titre des employés dont le taux de commission n est pas renseigné.
+.
+
+23 - Afficher le nom, le salaire, le taux de commission et le titre des employés dont le taux de commission est renseigné.
+.
+
+24 - Afficher le nom, le salaire, le taux de commission, le titre des employés dont le taux de commission est inférieur à 15.
+.
+
+25 - Afficher le nom, le salaire, le taux de commission, le titre des employés dont le taux de commission est supérieur à 15
+.
+
+26 - Afficher le nom, le salaire, le taux de commission et la commission des employés dont le taux de commission n est pas nul. 
+(la commission est calculée en multipliant le salaire par le taux de commission)
+.
+
+27 - Afficher le nom, le salaire, le taux de commission, la commission des employés dont le taux de commission n est pas nul, classé par taux de commission croissant.
+.
+
+28 - Afficher le nom et le prénom (concaténés) des employés. Renommer les colonnes
+.
+
+29 - Afficher les 5 premières lettres du nom des employés.
+.
+
+30 - Afficher le nom et le rang de la lettre « r » dans le nom des employés.
+.
+
+31 - Afficher le nom, le nom en majuscule et le nom en minuscule de l employé dont le nom est Vrante.
+.
+
+32 - Afficher le nom et le nombre de caractères du nom des employés
+.
