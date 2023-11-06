f49409fe7026149660fa7ce95e9f42e6ded0670c
camille <camille.cliche1@gmail.com>
2023-11-06 09:11:50 +0100

    e

---
+++ SQL/exemple-2.sql
@@ -0,0 +1,29 @@
+1 - Calculer le nombre d employés de chaque titre
+.
+
+2 - Calculer la moyenne des salaires et leur somme, par région.
+.
+
+3 - Afficher les numéros des départements ayant au moins 3 employés.
+.
+
+4 - Afficher les lettres qui sont l initiale d au moins trois employés.
+.
+
+5 - Rechercher le salaire maximum et le salaire minimum parmi tous les salariés et l écart entre les deux.
+.
+
+6 - Rechercher le nombre de titres différents
+.
+
+7 - Pour chaque titre, compter le nombre d employés possédant ce titre.
+.
+
+8 - Pour chaque nom de département, afficher le nom du département et le nombre d employés.
+.
+
+9 - Rechercher les titres et la moyenne des salaires par titre dont la moyenne est supérieure à la moyenne des salaires des Représentants.
+.
+
+10 - Rechercher le nombre de salaires renseignés et le nombre de taux de commission renseignés
+.
