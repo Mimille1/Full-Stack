6094146039482214f242b78c9ad131815d5818c5
mimille1 <camille.cliche1@gmail.com>
2023-11-10 10:26:24 +0100

    test

---
+++ SQL/exemple-3.sql
@@ -0,0 +1,3 @@
+INSERT INTO employe (noemp,nom,prenom,nodep,dateemb,titre,salaire) VALUES (26,'saint','catherine',50,09/11/2023,'employer',0) 
+INSERT INTO employe (noemp,nom,prenom,nodep,dateemb,titre,salaire) VALUES (27,'jean','michel',50,09/11/2023,'employer',0) 
+INSERT INTO employe (noemp,nom,prenom,nodep,dateemb,titre,salaire) VALUES (28,'eddie','michel',50,09/11/2023,'employer',0) 
