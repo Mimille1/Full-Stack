2a4d5d714d33b676b58b42f9ccf878072f4c35ae
Mimille1 <camille.cliche1@gmail.com>
2024-01-24 14:01:37 +0100

    new

---
+++ Full-Stack-main/PHP/Les boucles/Exercice_1.php
@@ -0,0 +1,6 @@
+    <?php
+    // Boucle pour afficher les nombres impairs entre 0 et 150
+    for ($i = 1; $i <= 150; $i += 2) {
+        echo $i . ' ';
+    }
+    ?>
---
+++ Full-Stack-main/SQL/exemple-2.sql
@@ -0,0 +1,62 @@
+Rechercher le prénom des employés et le numéro de la région de leur département.
+SELECT employe.prenom , dept.noregion FROM employe JOIN dept ON employe.nodep = dept.nodept
+
+Rechercher le numéro du département, le nom du département, le nom des employés classés par numéro de département (renommer les tables utilisées).
+.
+
+Rechercher le nom des employés du département Distribution
+.
+
+Rechercher le nom et le salaire des employés qui gagnent plus que leur patron, et le nom et le salaire de leur patron.
+.
+
+Rechercher le nom et le titre des employés qui ont le même titre que Amartakaldire.
+.
+
+Rechercher le nom, le salaire et le numéro de département des employés qui gagnent plus qu un seul employé du département 31, classés par numéro de département et salaire.
+.
+
+Rechercher le nom, le salaire et le numéro de département des employés qui gagnent plus que tous les employés du département 31, classés par numéro de département et salaire.
+.
+
+Rechercher le nom et le titre des employés du service 31 qui ont un titre que l on trouve dans le département 32.
+.
+
+Rechercher le nom et le titre des employés du service 31 qui ont un titre l on ne trouve pas dans le département 32.
+.
+
+Rechercher le nom, le titre et le salaire des employés qui ont le même titre et le même salaire que Fairant.
+.
+
+Rechercher le numéro de département, le nom du département, le nom des employés, en affichant aussi les départements dans lesquels il n y a personne, classés par numéro de département.
+.
+
+1 - Calculer le nombre d employés de chaque titre
+.
+
+2 - Calculer la moyenne des salaires et leur somme, par région.
+.
+
+3 - Afficher les numéros des départements ayant au moins 3 employés.
+.
+
+4 - Afficher les lettres qui sont l initiale d au moins trois employés.
+.
+
+5 - Rechercher le salaire maximum et le salaire minimum parmi tous les salariés et l écart entre les deux.
+.
+
+6 - Rechercher le nombre de titres différents
+.
+
+7 - Pour chaque titre, compter le nombre d employés possédant ce titre.
+.
+
+8 - Pour chaque nom de département, afficher le nom du département et le nombre d employés.
+.
+
+9 - Rechercher les titres et la moyenne des salaires par titre dont la moyenne est supérieure à la moyenne des salaires des Représentants.
+.
+
+10 - Rechercher le nombre de salaires renseignés et le nombre de taux de commission renseignés
+.
