857889b0a73b0987d2f8f7cb6dffa005fe2209bc
Mimille1 <camille.cliche1@gmail.com>
2024-01-25 18:26:10 +0100

    record

---
+++ PDO/Record/co_BD.php
@@ -0,0 +1,7 @@
+<?php
+try {
+    $db = new PDO('mysql:host=localhost:3307;dbname=record', 'root', '');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    echo "Erreur de connexion" . $e->getMessage();
+}
