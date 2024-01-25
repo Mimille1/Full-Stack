07fb0d18676c8c79063f577af4b4c54b28aade61
Mimille1 <camille.cliche1@gmail.com>
2024-01-25 13:51:34 +0100

    record

---
+++ Record/db.php
@@ -0,0 +1,11 @@
+<?php
+    try 
+    {        
+        $db = new PDO('mysql:host=localhost:3307;dbname=record', 'root', '');
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (Exception $e) {
+        echo "Erreur : " . $e->getMessage() . "<br>";
+        echo "N° : " . $e->getCode();
+        die("Fin du script");
+    }       
+?>
