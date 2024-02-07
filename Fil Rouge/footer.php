6e5b12e22fbadf3272d1d3eb467a114d306bacc3
Mimille1 <camille.cliche1@gmail.com>
2024-02-07 10:32:17 +0100

    TD

---
+++ Fil Rouge/footer.php
@@ -0,0 +1,35 @@
+<footer>
+    <div class="fixed-bottom overflow-hidden">
+
+        <div class="row">
+            <div class="row col-12 justify-content-md-center d-none d-md-flex mt-5">
+
+                <img class="img-fluid" src="ASSET/the_district_brand/facebook logo.png" class="img-fluid" alt="facebook" style="height: 4rem; width: auto;">
+
+                <img class="img-fluid" src="ASSET/the_district_brand/instagram logo.jpeg" class="img-fluid" alt="instagram" style="height: 4rem; width: auto;">
+
+                <img class="img-fluid" src="ASSET/the_district_brand/linkedin logo.png" class="img-fluid" alt="linkedin" style="height: 4rem; width: auto;">
+
+                <img class="img-fluid" src="ASSET/the_district_brand/pinterest logo.png" class="img-fluid" alt="pinterest" style="height: 4rem; width: auto;">
+
+                <img class="img-fluid" src="ASSET/the_district_brand/twitter logo.png" class="img-fluid" alt="twitter" style="height: 4rem; width: auto;">
+
+                <img class="img-fluid" src="ASSET/the_district_brand/youtube logo.jpeg" class="img-fluid" alt="youtube" style="height: 4rem; width: auto;">
+
+            </div>
+            <a href="confidentialite.php"><b><u>Politique de confidentialité&nbsp;&nbsp;</u></b></a>
+            <a href="mention.php"><b><u>Mention légale</u></b></a>
+
+
+        </div>
+
+    </div>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="asset/javascript/ajax.js"></script>
+    <script src="asset/javascript/fil_rouge.js"></script>
+</footer>
